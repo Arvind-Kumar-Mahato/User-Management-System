b97dcb7566f578d12939a839347e9b12aa3f1a80
Arvind-Kumar-Mahato <93374171+Arvind-Kumar-Mahato@users.noreply.github.com>
2022-07-24 11:47:15 +0530

    MongoDb connection success

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyparser =  require('body-parser');
 const path = require('path');
 
+const connectDB = require('./server/database/connection');
+
 const { urlencoded } = require('body-parser');
 const app = express();
 dotenv.config({path:'config.env'})
@@ -12,7 +14,8 @@ const PORT = process.env.PORT || 8080
 //log request 
 app.use(morgan('tiny'));
 
-
+//mongoBD connection 
+connectDB();
 //parse request to bodyparser
 app.use(bodyparser.urlencoded({extended:true}))
 
--- server/routes/router.js
+++ server/routes/router.js
@@ -3,6 +3,8 @@ const route = express.Router()
 
 
 const services = require('../services/render');
+
+const controller = require("../controller/controller");
 /**
  * @description Root Route
  * @method GET/
@@ -23,6 +25,12 @@ route.get('/add-user',services.add_user)
  */
 
 route.get('/update-user',services.update_user)
+
+//API
+route.post('/api/users',controller.create);
+route.get('/api/users',controller.find);
+route.put('/api/users:id',controller.update);
+route.delete('/api/users:id',controller.delete);
     
 
 module.exports = route
